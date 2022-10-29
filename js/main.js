85e13b5e951aed50fb0984b63283cb1975e63495
naokisum <71647303+naokisum@users.noreply.github.com>
2022-10-30 04:46:42 +0900

    rakus

--- js/main.js
+++ js/main.js
@@ -3,4 +3,8 @@ $(function() {
         $('.hamburger').toggleClass('active');
         $('#page').toggleClass('slide-right');
     });
+    $('#page li a').on('click', function() {
+        $('.hamburger').toggleClass('active');
+        $('#page').toggleClass('slide-right');
+    });
 });
